349a0a4d097422b9126244eaf3b319e181084c79
Ali Moradi <Alimoradi0business@gmail.com>
2024-02-18 12:47:33 -0300

    some features added to buttons 😋

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -4,8 +4,11 @@ import { Link } from "react-router-dom";
 
 export function MainButton(props) {
   return (
-    <div className="my-3 w-[250px] h-[50px] bg-[#FF2E00] text-xl rounded-2xl flex justify-center items-center text-[#131313]">
-      <button className="font-inter-bold" onClick={props.clickHandler}>
+    <div className="py-3 px-3 my-3 w-[150px] h-[60px] sm:w-[250px] sm:h-[50px] bg-[#FF2E00] text-xl rounded-2xl flex justify-center items-center text-[#131313] hover:bg-[#131313] hover:border hover:border-[#FF2E00] hover:text-[#fff] transition-all">
+      <button
+        className="font-inter-bold font-bold"
+        onClick={props.clickHandler}
+      >
         {!props.path ? (
           props.content
         ) : (
@@ -18,7 +21,7 @@ export function MainButton(props) {
 
 export function SecondryButton(props) {
   return (
-    <div className="my-3 w-[250px] h-[50px] bg-[#131313] text-xl border border-[#FF2E00] rounded-2xl flex justify-center items-center text-[#FF2E00]">
+    <div className="py-3 px-3 my-3 w-[150px] h-[60px] sm:w-[250px] sm:h-[50px] bg-[#131313] text-xl border border-[#FF2E00] rounded-2xl flex justify-center items-center text-[#FF2E00] hover:bg-[#ff2f001f] hover:text-[#fff] transition-all">
       <button className="font-inter-bold" onClick={props.clickHandler}>
         {!props.path ? (
           props.content
