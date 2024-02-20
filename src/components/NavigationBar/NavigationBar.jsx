611b128285c50d0c6331614131c7c82acdca4598
Ali Moradi <Alimoradi0business@gmail.com>
2024-02-20 15:44:36 -0300

    responsive navbar done 😍

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -7,8 +7,13 @@ import { Link, NavLink } from "react-router-dom";
 import { getUserDataApi } from "../../setting/Funcs/API";
 import { getUserData } from "../../setting/Funcs/funcs";
 
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoClose } from "react-icons/io5";
+
 export default function NavigationBar() {
   const [userName, setUserName] = useState("");
+
+  const [showMenu, setShowMenu] = useState(false);
 
   useEffect(() => {
     getUserData(
@@ -22,7 +27,7 @@ export default function NavigationBar() {
 
   return (
     <header>
-      <div className="container py-7">
+      <div className="container py-7 relative">
         <div className="flex items-center justify-between">
           <div className="flex items-center justify-between">
             <div className="w-10 h-10 ml-4 mr-10">
@@ -30,7 +35,7 @@ export default function NavigationBar() {
                 <img src={siteLogo} alt="img" />
               </Link>
             </div>
-            <nav className="text-white flex items-center gap-8 child:transition-all">
+            <nav className="text-white hidden md:flex items-center gap-8 child:transition-all">
               <NavLink
                 className={(link) =>
                   link.isActive
@@ -73,7 +78,7 @@ export default function NavigationBar() {
               </NavLink>
             </nav>
           </div>
-          <div className="py-1 px-10 bg-[#131313] text-xl border border-[#FF2E00] rounded-2xl flex justify-center items-center text-[#FF2E00] hover:bg-[#ff2f001f] hover:text-[#fff] transition-all">
+          <div className="hidden py-1 px-10 bg-[#131313] text-xl border border-[#FF2E00] rounded-2xl md:flex justify-center items-center text-[#FF2E00] hover:bg-[#ff2f001f] hover:text-[#fff] transition-all">
             <button>
               <Link
                 className="text-white capitalize font-inter-bold font-bold"
@@ -83,7 +88,63 @@ export default function NavigationBar() {
               </Link>
             </button>
           </div>
+          <div className="md:hidden">
+            <div>
+              <RxHamburgerMenu
+                onClick={() => {
+                  setShowMenu(true);
+                }}
+                className="text-white"
+                size="1.8em"
+              />
+            </div>
+          </div>
         </div>
+        <div
+          className={`${
+            showMenu ? "opacity-1 visible" : "opacity-0 invisible"
+          } absolute top-0 bottom-0 right-0 bg-[#131313] w-64 h-[100dvh] rounded-xl border-l-4 px-8 py-4 divide-y-2 transition-all z-50`}
+        >
+          <div className="flex items-center justify-between">
+            <button
+              onClick={() => {
+                setShowMenu(false);
+              }}
+            >
+              <IoClose className="text-white" size="1.5em" />
+            </button>
+            <div className="py-1.5 px-4 bg-[#131313] text-xl border border-[#FF2E00] rounded-2xl md:flex justify-center items-center text-[#FF2E00] hover:bg-[#ff2f001f] hover:text-[#fff] transition-all">
+              <button>
+                <Link
+                  className="text-white capitalize font-inter-bold font-bold"
+                  to="/dashboard"
+                >
+                  {userName}
+                </Link>
+              </button>
+            </div>
+          </div>
+          <nav className="text-white mt-3 pt-5 flex flex-col justify-center gap-4">
+            <Link className="px-1 py-0.5" to="/">
+              Home
+            </Link>
+            <Link className="px-1 py-0.5" to="/list">
+              Songs List
+            </Link>
+            <Link className="px-1 py-0.5" to="/uploadfile">
+              Upload Song
+            </Link>
+            <Link className="px-1 py-0.5" to="/aboutus">
+              About Us
+            </Link>
+          </nav>
+        </div>
+
+        <div
+          className={`${
+            showMenu ? "opacity-1 visible" : "opacity-0 invisible"
+          } w-screen h-[100dvh] fixed top-0 right-0 left-0 bottom-0 z-10 backdrop-blur-sm transition-all duration-300`}
+        ></div>
       </div>
     </header>
   );
